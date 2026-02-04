cd869984935c1e5decfa763bf94c71a62558b703
David Monaghan <monaghan.david@gmail.com>
2026-02-04 10:14:17 -0500

    fix: update Playwright configuration to use 'npm run dev' and change port to 4277

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,14 +2,14 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173,
+		command: 'npm run dev',
+		port: 4277,
 		reuseExistingServer: !process.env.CI
 	},
 	testDir: 'tests/e2e',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	use: {
-		baseURL: 'http://localhost:4173',
+		baseURL: 'http://localhost:4277',
 		trace: 'retain-on-failure',
 		screenshot: 'only-on-failure'
 	},
